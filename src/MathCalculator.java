d3a7bd055effdfde6ff1b728da1f99197b3d01b0
minhhieu99a <hieunb99@gmail.com>
2022-03-08 11:08:49 +0700

    done

---
+++ src/MathCalculator.java
@@ -0,0 +1,20 @@
+public class MathCalculator implements Calculator{
+    public double add(double first, double second) {
+        return first + second;
+    }
+
+    @Override
+    public double sub(double first, double second) {
+        return first - second;
+    }
+
+    @Override
+    public double mul(double first, double second) {
+        return first * second;
+    }
+
+    @Override
+    public double div(double first, double second) {
+        return first / second;
+    }
+}
